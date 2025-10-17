36b01aee89b39014caf6b944378c6db3b810d458
Valerio Di Felice <valesmile.99@gmail.com>
2025-10-17 10:48:58 +0200

    Initial commit

---
+++ U5-W6-D5/src/test/java/com/example/U5_W6_D5/U5W6D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U5_W6_D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W6D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
